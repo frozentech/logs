bf8071a1c5cdb341234f7b96e23c2b9274b2deaa
Lester Soriano <lestersoriano@yahoo.com>
2021-04-20 11:37:33 +0800

    Update log.go

--- log.go
+++ log.go
@@ -2,175 +2,66 @@ package logs
 
 import (
 	"fmt"
-	"os"
-	"strconv"
 	"strings"
 	"time"
-
-	"github.com/willf/pad"
 )
 
-const (
-	// NewInstanceMsg sets the message to indicate the start of the log
-	NewInstanceMsg = "START"
-	// EndInstanceMsg sets the message to indicate the end of the log
-	EndInstanceMsg = "END"
-	// LogLevelDebug defines a normal debug log
-	LogLevelDebug = "DEBUG"
-	// LogLevelPanic defines a panic log
-	LogLevelPanic = "PANIC"
-	// LogLevelFatal defines a fatal log
-	LogLevelFatal = "FATAL"
-	// DateFormat defines the log date format
-	DateFormat = time.RFC3339
-)
-
-var (
-	osExit = os.Exit
-
-	// Story  log
-	Story *Log
-)
-
-// Log represents information about a rest server log.
-type Log struct {
-	entries   []Entry
-	folder    string
-	Pad       int
-	startTime int64
-}
-
-// Entry represents information about a rest server log entry.
+// Entry ...
 type Entry struct {
-	Level   string
-	Message string
-	Time    time.Time
+	Name    string      `json:"name"`
+	Message interface{} `json:"message"`
+	Time    time.Time   `json:"timestamp"`
 }
 
-func (l Log) getDate(t time.Time) string {
-	return t.Format(DateFormat + " MST")
-}
+// Stories ...
+var Stories *Logs
 
-// Padding ...
-func (l *Log) Padding(i int) {
-	l.Pad = i
-	return
-}
-
-// New creates new instance of Log
-func New() *Log {
-	var log Log
-
-	log.entries = make([]Entry, 1)
-	log.entries[0] = Entry{
-		Level:   LogLevelDebug,
-		Message: NewInstanceMsg,
-		Time:    time.Now().UTC(),
+// Record ...
+func Record(name string, v interface{}) {
+	if Stories != nil {
+		Stories.Push(name, v)
 	}
+}
 
-	log.Pad = 30
-	log.startTime = log.TimeMs()
+// Logs ...
+type Logs struct {
+	Entries []Entry
+}
 
+// New ...
+func New() *Logs {
+	var log Logs
+	log.Entries = make([]Entry, 0)
 	return &log
 }
 
-func (l *Log) addEntry(level string, v ...interface{}) {
-	l.entries = append(
-		l.entries,
+// Push ...
+func (l *Logs) Push(name string, v interface{}) {
+	l.Entries = append(
+		l.Entries,
 		Entry{
-			Level:   level,
-			Message: fmt.Sprint(v...),
-			Time:    time.Now(),
+			Name:    name,
+			Message: v,
+			Time:    time.Now().UTC(),
 		},
 	)
 }
 
-// Entries returns all the entries
-func (l *Log) Entries() []Entry {
-	return l.entries
+func format(name string) string {
+	return strings.Replace(name, " ", "_", -1)
 }
 
-// Record ...
-func (l *Log) Record(header string, v interface{}) error {
-	l.Print(pad.Right(header, l.Pad, " "), v)
-	return nil
-}
+// Dump ...
+func (l *Logs) Dump() (fields map[string]interface{}) {
+	fields = make(map[string]interface{}, 0)
 
-// Print a regular log
-func (l *Log) Print(v ...interface{}) {
-	l.addEntry(LogLevelDebug, v...)
-}
-
-// Panic then throws a panic with the same message afterwards
-func (l *Log) Panic(v ...interface{}) {
-	l.addEntry(LogLevelPanic, v...)
-	panic(fmt.Sprint(v...))
-}
-
-// LastEntry returns the last inserted log
-func (l *Log) LastEntry() Entry {
-	return l.entries[len(l.entries)-1]
-}
-
-// Count returns number of inserted logs
-func (l *Log) Count() int {
-	return len(l.entries)
-}
-
-// TimeMs Time in Micro Second
-func (l *Log) TimeMs() int64 {
-	return time.Now().UnixNano() / 1000000
-}
-
-// Dump will print all the messages to the io.
-func (l *Log) Dump(printLogs ...bool) string {
-	var (
-		line, format string
-		params       []interface{}
-		messages     []string
-		print        bool
-	)
-
-	if len(printLogs) > 0 {
-		print = printLogs[0]
-	} else {
-		print = true
-	}
-
-	l.Print("Elapse ", l.TimeMs()-l.startTime, " ms")
-	l.addEntry(LogLevelDebug, EndInstanceMsg+"\n")
-
-	len := len(l.entries)
-	for i := 0; i < len; i++ {
-		format = "%s\t%s"
-		params = []interface{}{
-			l.getDate(l.entries[i].Time),
-			l.entries[i].Level,
+	for idx, item := range l.Entries {
+		if item.Message == nil {
+			fields[fmt.Sprintf("%06d", idx)] = item.Name
+		} else {
+			fields[fmt.Sprintf("%06d.%s", idx, item.Name)] = item.Message
 		}
-
-		params = append(params, pad.Left(strconv.Itoa(i), 3, "0"))
-		format = format + "  %s"
-
-		params = append(params, l.entries[i].Message)
-
-		format = format + "  %s\n"
-		line = fmt.Sprintf(format, params...)
-
-		if print {
-			fmt.Print(line)
-		}
-		messages = append(messages, line)
-
 	}
 
-	l.entries = make([]Entry, 1)
-	l.entries[0] = Entry{
-		Level:   LogLevelDebug,
-		Message: NewInstanceMsg,
-		Time:    time.Now(),
-	}
-
-	l.startTime = l.TimeMs()
-
-	return strings.Join(messages, "\n")
+	return
 }
